0426c6745759fb66220993053024ac62ff7ae165
Cosmos257 <roopapramodan@gmail.com>
2025-09-09 13:52:36 +0530

    Update scoreboard.py

--- src/cocotbext/lin/scoreboard.py
+++ src/cocotbext/lin/scoreboard.py
@@ -1,14 +1,23 @@
+from utils import build_expected_header, build_expected_response
+from cocotb.triggers import RisingEdge, ReadOnly
+
 class LinScoreboard:
-    def __init__(self, dut, utils):
+    def __init__(self, dut):
         self.dut = dut
-        self.utils = utils
 
-    async def check_header(self, pid):
-        actual = int(self.dut.frame_header_out.value)
-        expected, _ = self.utils.build_expected_header(pid)
-        assert actual == expected, f"Header mismatch: {hex(actual)} != {hex(expected)}"
+    async def check(self, transaction, i):
+        # Header
+        await RisingEdge(self.dut.comm_tx_done)
+        await ReadOnly()
+        actual_header = int(self.dut.frame_header_out.value)
+        expected_header, _ = build_expected_header(transaction.pid)
+        assert actual_header == expected_header, \
+            f"[Header Mismatch] Tx {i}: {hex(actual_header)} != {hex(expected_header)}"
 
-    async def check_response(self, data_bytes):
-        actual = int(self.dut.response_out.value)
-        expected, _ = self.utils.build_expected_response(data_bytes)
-        assert actual == expected, f"Response mismatch: {hex(actual)} != {hex(expected)}"
+        # Response
+        await RisingEdge(self.dut.resp_tx_done)
+        await ReadOnly()
+        actual_response = int(self.dut.response_out.value)
+        expected_response, _ = build_expected_response(transaction.data_bytes + [0] * (8 - len(transaction.data_bytes)))
+        assert actual_response == expected_response, \
+            f"[Response Mismatch] Tx {i}: {hex(actual_response)} != {hex(expected_response)}"
