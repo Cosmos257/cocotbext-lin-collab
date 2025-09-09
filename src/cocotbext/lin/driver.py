1dfbf63e554a22e4cbdb694c1997d157dc653019
Cosmos257 <roopapramodan@gmail.com>
2025-09-09 13:51:46 +0530

    Update driver.py

--- src/cocotbext/lin/driver.py
+++ src/cocotbext/lin/driver.py
@@ -1,18 +1,14 @@
-import random
+import cocotb
 from cocotb.triggers import RisingEdge
 
-
 class LinDriver:
-    def __init__(self, dut, clock):
+    def __init__(self, dut):
         self.dut = dut
-        self.clock = clock
 
-    async def send(self, pid, data_bytes):
-        # Pad to 8 bytes
-        padded_data = data_bytes + [0] * (8 - len(data_bytes))
-        self.dut.pid.value = pid
-        self.dut.data.value = int.from_bytes(bytes(padded_data), "little")
-        await RisingEdge(self.clock)
-        return padded_data
+    async def send(self, transaction):
+        padded = transaction.data_bytes + [0] * (8 - len(transaction.data_bytes))
+        self.dut.pid.value = transaction.pid
+        self.dut.data.value = int.from_bytes(bytes(padded), "little")
+        await RisingEdge(self.dut.sys_clk)
 
 
