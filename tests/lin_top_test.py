7f45b87ffc05ed712d00cd184d8ba3703e312787
Cosmos257 <roopapramodan@gmail.com>
2025-09-09 13:53:11 +0530

    Update lin_top_test.py

--- tests/lin_top_test.py
+++ tests/lin_top_test.py
@@ -1,57 +1,38 @@
+# lin_top_test.py
+
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import Timer, RisingEdge, ReadOnly
-import random
+from cocotb.triggers import Timer
 from cocotb_coverage.coverage import coverage_db
+import os
 
-from cocotbext.lin.driver import LinDriver
-from cocotbext.lin.monitor import LinMonitor
-from cocotbext.lin.scoreboard import LinScoreboard
-import cocotbext.lin.utils as utils
-
-from cocotbext.lin.coverage import cover_pid, cover_data_byte, cover_data_len, protocol_cover
+from bus import LinTransaction
+from driver import LinDriver
+from monitor import protocol_monitor
+from scoreboard import LinScoreboard
+from config import random_pid, random_data
 
 
 @cocotb.test()
 async def lin_top_test(dut):
-    # Reset
     dut.rstn.value = 1
     await Timer(1, "ns")
     dut.rstn.value = 0
     await Timer(1, "ns")
-    await RisingEdge(dut.sys_clk)
     dut.rstn.value = 1
 
-    # Clock
-    cocotb.start_soon(Clock(dut.sys_clk, 10, "ns").start())
+    cocotb.start_soon(protocol_monitor(dut))
+    driver = LinDriver(dut)
+    scoreboard = LinScoreboard(dut)
 
-    # Init VIP
-    driver = LinDriver(dut, dut.sys_clk)
-    scoreboard = LinScoreboard(dut, utils)
-    monitor = LinMonitor(dut, callback=protocol_cover)
-    cocotb.start_soon(monitor.run())
+    for i in range(30):
+        pid = random_pid()
+        length, data_bytes = random_data()
+        txn = LinTransaction(pid, data_bytes)
 
-    for i in range(20):
-        pid = random.randint(0, 0x3F)
-        data_len = random.randint(1, 8)
-        data_bytes = [random.randint(0, 0xFF) for _ in range(data_len)]
-
-        cover_pid(pid)
-        cover_data_len(data_len)
-        for b in data_bytes:
-            cover_data_byte(b)
-
-        padded = await driver.send(pid, data_bytes)
-
-        await RisingEdge(dut.comm_tx_done)
-        await ReadOnly()
-        await scoreboard.check_header(pid)
-
-        await RisingEdge(dut.resp_tx_done)
-        await ReadOnly()
-        await scoreboard.check_response(padded)
-
+        await driver.send(txn)
+        await scoreboard.check(txn, i+1)
         await Timer(50, "ns")
 
     coverage_db.report_coverage(cocotb.log.info, bins=True)
-    coverage_db.export_to_xml("coverage.xml")
+    coverage_file = os.path.join(os.getenv("RESULT_PATH", "./"), "coverage.xml")
+    coverage_db.export_to_xml(filename=coverage_file)
