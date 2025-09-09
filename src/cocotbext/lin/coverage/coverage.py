3fb6fe08144bcd58d8b0a69a64ef27967aeb64a1
Cosmos257 <roopapramodan@gmail.com>
2025-09-09 17:27:12 +0530

    Update coverage.py

--- src/cocotbext/lin/coverage/coverage.py
+++ src/cocotbext/lin/coverage/coverage.py
@@ -1,19 +1,26 @@
 from cocotb_coverage.coverage import CoverPoint, CoverCross
 
+PID_LOW_THRESHOLD = 21
+PID_MEDIUM_THRESHOLD = 42
+BYTE_LOW_THRESHOLD = 85
+BYTE_MEDIUM_THRESHOLD = 170
+
+
 @CoverPoint("top.prot.lin.current", xf=lambda t: t["current"], bins=["Idle","HeaderTx","ResponseTx"])
 @CoverPoint("top.prot.lin.previous", xf=lambda t: t["previous"], bins=["Idle","HeaderTx","ResponseTx"])
 @CoverCross("top.prot.lin.cross", items=["top.prot.lin.previous","top.prot.lin.current"])
 def protocol_cover(t):
     pass
 
-@CoverPoint("top.lin.pid", xf=lambda pid: "low" if pid < 21 else ("medium" if pid < 42 else "high"), bins=["low", "medium", "high"])
+@CoverPoint("top.lin.pid", xf=lambda pid: "low" if pid < PID_LOW_THRESHOLD else ("medium" if pid < PID_MEDIUM_THRESHOLD  else "high"), bins=["low", "medium", "high"])
 def cover_pid(pid):
     pass
 
-@CoverPoint("top.lin.data_byte", xf=lambda b: "low" if b < 85 else ("medium" if b < 170 else "high"), bins=["low", "medium", "high"])
+@CoverPoint("top.lin.data_byte", xf=lambda b: "low" if b < BYTE_LOW_THRESHOLD else ("medium" if b < BYTE_MEDIUM_THRESHOLD  else "high"), bins=["low", "medium", "high"])
 def cover_data_byte(b):
     pass
 
 @CoverPoint("top.lin.data_len", xf=lambda length: length, bins=list(range(1, 9)))
 def cover_data_len(length):
     pass
+
