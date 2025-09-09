d3e5a208b21a6708a4f9dd16588b3f2ba752db75
Cosmos257 <roopapramodan@gmail.com>
2025-09-09 13:51:20 +0530

    Update bus.py

--- src/cocotbext/lin/bus.py
+++ src/cocotbext/lin/bus.py
@@ -1,10 +1,4 @@
-class LinBus:
-    def __init__(self, dut):
-        self.clk = dut.sys_clk
-        self.rstn = dut.rstn
-        self.pid = dut.pid
-        self.data = dut.data
-        self.comm_tx_done = dut.comm_tx_done
-        self.resp_tx_done = dut.resp_tx_done
-        self.frame_header_out = dut.frame_header_out
-        self.response_out = dut.response_out
+class LinTransaction:
+    def __init__(self, pid, data_bytes):
+        self.pid = pid
+        self.data_bytes = data_bytes
